691d6865c10c5ba69654339eb300cf3af0b691eb
lafaetehmiranda <39806882+lafaetehmiranda@users.noreply.github.com>
2024-10-23 10:37:56 +0100

    Add files via upload

---
+++ api-device-manager /src/main/java/com/device/manager/api_device_manager/ApiDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.device.manager.api_device_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeviceManagerApplication.class, args);
+	}
+
+}
---
+++ api-device-manager /src/test/java/com/device/manager/api_device_manager/ApiDeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.device.manager.api_device_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
